a5fdd9b3412fa16a745648feda43bffdc1d1a544
0x6c6868 <1023347025@qq.com>
2021-05-07 12:30:48 +0800

    upgrade to py3

--- FindMyPassword.py
+++ FindMyPassword.py
@@ -1,30 +1,29 @@
-#!/usr/bin/env python
-#-*- coding:utf-8 -*-
+#!/usr/bin/env python3
 
 """
 这个项目是干嘛的？
-防止自己注册的小网站被黑了，我的全网账户全炸了用的。
+防止自己注册的小网站被黑了，我的全网账户密码全炸了。
 """
 
 import hashlib, sys, getopt
 
 def md5_passwd(mask, sitename, sitevalue):
     m = hashlib.md5()
-    m.update(mask)
-    m.update(sitevalue)
+    m.update(mask.encode('utf8'))
+    m.update(sitevalue.encode('utf8'))
 
     # 首字母务必大写
     res = m.hexdigest()[:12]
-    print sitename + '\t -> \t' +\
+    print(sitename + '\t -> \t' +\
           res[:1].title() +\
-          res[1:]
+          res[1:])
 
 if __name__ == '__main__':
 
     opts, args = getopt.getopt(sys.argv[1:], "hp:s:")
 
     if opts is None or len(opts) == 0:
-        print "usage: ./FindMyPassword.py -p yourpasswordmask"
+        print("usage: ./FindMyPassword.py -p yourpasswordmask")
         sys.exit()
 
     for op, value in opts:
@@ -33,7 +32,7 @@ if __name__ == '__main__':
             mask = value
 
         if op == "-h":
-            print "usage: ./FindMyPassword.py -p yourpasswordmask"
+            print("usage: ./FindMyPassword.py -p yourpasswordmask")
             sys.exit()
 
     sites = {
